0c7cafe2856b6db3766fc7817fd5a58e8247a3f3
Wangabai <1059917258@qq.com>
2021-03-04 20:43:31 +0800

    wancheng denglu

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<!-- Home组件 -->
+<template>
+  <div>
+    <el-button
+      type="info"
+      @click="logout"
+    >
+      退出
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    //   退出按钮
+    logout () {
+      // 清空token
+      window.sessionStorage.clear()
+      //   跳转到登录也
+      this.$router.push('./login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>

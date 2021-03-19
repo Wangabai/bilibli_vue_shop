acf6d1c551d30cc4939dcd983610f57d187c37fd
Wangabai <1059917258@qq.com>
2021-03-19 17:56:56 +0800

    Order completion function

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import Add from '../components/goods/Add.vue'
 import GoodsList from '../components/goods/List.vue'
+import Order from '../components/order/Order.vue'
 
 Vue.use(VueRouter)
 
@@ -59,6 +60,10 @@ const routes = [
       {
         path: '/goods',
         component: GoodsList
+      },
+      {
+        path: '/orders',
+        component: Order
       }
     ]
   }
